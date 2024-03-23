ca5aa5069074035ec66ca3caa14cad0332e33830
Wilson Watson <wwatso1tech@gmail.com>
2024-03-23 14:32:43 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,10 @@ pub mod topic;
 pub mod client_config;
 pub mod client;
 
+pub use message_type::*;
+pub use messages::*;
+pub use subscription::*;
+pub use topic::*;
+
+pub use client::Client;
+pub use client_config::Config;
